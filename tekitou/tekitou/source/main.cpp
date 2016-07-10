4c08e42b9adc5dd4235b9ac1d9fae80b81b5335d
syuki <sn.tktkdog@gmail.com>
2016-07-10 12:13:12 +0900

    ウィンドウサイズに非クライアント領域計算追加

--- tekitou/tekitou/source/main.cpp
+++ tekitou/tekitou/source/main.cpp
@@ -7,6 +7,13 @@
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
 
   SingleWindowCreator window_creator;
+  window_creator.EditCreateWindowExParameter([](CreateWindowExParameter& param) {
+    // 非クライアント領域のサイズ計算
+    RECT client_rect = { 0, 0, 800, 600 };
+    AdjustWindowRect(&client_rect, param._window_style, false);
+    param._window_width = client_rect.right - client_rect.left;
+    param._window_height = client_rect.bottom - client_rect.top;
+  });
   window_creator.Create(hInstance, nCmdShow);
 
   WindowMessageLooper::GetInstance()->MessageLoop([]() {});
